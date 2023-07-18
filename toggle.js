c3be53e3c2dbcb2ee11dd8e3babf25cd19e6d55e
Guido <guidotoritto@outlook.com>
2023-07-18 16:56:08 -0300

    dropdowns

--- toggle.js
+++ toggle.js
@@ -2,7 +2,8 @@ var toggle = document.getElementById('button')
 var body = document.querySelector('body')
 
 toggle.onclick = function(){
-    toggle.classList.toggle('active');
-    body.classList.toggle('active');
+    toggle.classList.toggle('active')
+    body.classList.toggle('active')
 }
+
 
